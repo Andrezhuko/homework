d18de33027a64bb6f176c69a3125052687127fe3
andre zhuko <andrey_zhu_kov@mail.ru>
2025-09-03 23:39:10 +0500

    add new function

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,8 @@ def filter_by_state(list_dicts: list[dict], state: str='EXECUTED') -> list[dict]
         if dicts['state'] == state:
             new_list_dicts.append(dicts)
     return new_list_dicts
+
+def sort_by_date(list_dicts: list[dict], ascending: bool=True) -> list[dict]:
+    """функция которая сортирует список словарей по дате"""
+    new_list_dicts = sorted(list_dicts, key=lambda x: x['date'], reverse=ascending)
+    return new_list_dicts
