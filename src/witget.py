195ea58bce7994263cacfa1f384d4e8e5633b4e8
andre zhuko <andrey_zhu_kov@mail.ru>
2025-09-08 09:46:19 +0500

    add tests

--- src/witget.py
+++ src/witget.py
@@ -13,7 +13,7 @@ def mask_account_card(count: str) -> str:
         else:
             list_names.append(i)
 
-    if len(list_numbers) == 20:
+    if len(list_numbers) > 16:
         return f'{"".join(list_names)}{(get_mask_account("".join(list_numbers)))}'
 
     else:
