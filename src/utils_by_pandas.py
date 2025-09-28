ac91226f64815202f5950a76ea21749d0a11d263
andre zhuko <andrey_zhu_kov@mail.ru>
2025-09-28 23:02:58 +0500

    fixes

--- src/utils_by_pandas.py
+++ src/utils_by_pandas.py
@@ -6,11 +6,9 @@ import csv
 
 def by_prossering_csv(link_by_csv_file: str) -> Any:
     """функция которая принимает файл ссв а возвращает список словарей"""
-    if type(link_by_csv_file) == str:
-        with open(link_by_csv_file, "r", encoding="utf-8") as file:
-            return [a for a in csv.DictReader(file, delimiter=';')]
-    else:
-        return "неправильный формат ссылки"
+    with open(link_by_csv_file, "r", encoding="utf-8") as file:
+        return [a for a in csv.DictReader(file, delimiter=';')]
+
 
 def by_prossering_exe(link_by_exe_file: str) -> Any:
     """фукнция которая принмает эксэль а возвращает список словарей"""
@@ -18,4 +16,3 @@ def by_prossering_exe(link_by_exe_file: str) -> Any:
             return [a for a in pd.read_excel(link_by_exe_file)]
     else:
         return "неправильный формат ссылки"
-
