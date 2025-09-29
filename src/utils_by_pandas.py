21887eb09fb9d8eda4f1f2c25fc1d8c6210aa77f
andre zhuko <andrey_zhu_kov@mail.ru>
2025-09-29 22:43:34 +0500

    fixes

--- src/utils_by_pandas.py
+++ src/utils_by_pandas.py
@@ -4,22 +4,17 @@ import pandas as pd
 
 
 def by_processing_csv(link_by_csv_file: str) -> Any:
-    """функция которая принимает файл ссв а возвращает список словарей"""
-    try:
-        if isinstance(link_by_csv_file, str):
-            return "неверный формат файла или файл отсутсвует"
-        else:
-            return (pd.read_csv(link_by_csv_file)).to_dict(orient="records")
-    except Exception as error:
-        return f"ошибка {error}"
+    """функция которая принимает файл csv а возвращает список словарей"""
+    if isinstance(link_by_csv_file, str):
+        raise FileNotFoundError("неверный формат файла или файл отсутсвует")
+    else:
+        return (pd.read_csv(link_by_csv_file)).to_dict(orient="records")
+
 
 
 def by_processing_exe(link_by_exe_file: str) -> Any:
-    """фукнция которая принмает эксэль а возвращает список словарей"""
-    try:
-        if isinstance(link_by_exe_file, str):
-            return "неверный формат файла или файл отсутсвует"
-        else:
-            return (pd.read_excel(link_by_exe_file)).to_dict(orient="records")
-    except Exception as error:
-        return f"ошибка {error}"
+    """фукнция которая принмает exe а возвращает список словарей"""
+    if isinstance(link_by_exe_file, str):
+        raise FileNotFoundError("неверный формат файла или файл отсутсвует")
+    else:
+        return (pd.read_excel(link_by_exe_file)).to_dict(orient="records")
