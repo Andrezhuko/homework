e491c7aaf4aba3f6e83f3414a19f6ecac49f4985
andre zhuko <andrey_zhu_kov@mail.ru>
2025-09-29 21:55:56 +0500

    fixes

--- src/utils_by_pandas.py
+++ src/utils_by_pandas.py
@@ -3,7 +3,7 @@ from typing import Any
 import pandas as pd
 
 
-def by_prossering_csv(link_by_csv_file: str) -> Any:
+def by_processing_csv(link_by_csv_file: str) -> Any:
     """функция которая принимает файл ссв а возвращает список словарей"""
     try:
         if isinstance(link_by_csv_file, str):
@@ -14,7 +14,7 @@ def by_prossering_csv(link_by_csv_file: str) -> Any:
         return f"ошибка {error}"
 
 
-def by_prossering_exe(link_by_exe_file: str) -> Any:
+def by_processing_exe(link_by_exe_file: str) -> Any:
     """фукнция которая принмает эксэль а возвращает список словарей"""
     try:
         if isinstance(link_by_exe_file, str):
