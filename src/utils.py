7900c2d9fdc2d6c1b45e4d46314384ab04439fed
andre zhuko <andrey_zhu_kov@mail.ru>
2025-10-22 23:28:12 +0500

    fixes

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("logs/utils.log", "w", "utf-8")
+file_handler = logging.FileHandler("../logs/utils.log", "w", "utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
